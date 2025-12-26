82abd86e56483d9f8fc56a1f7ada0ec2e7ed76cc
Almonteco Enterprises <39785552+almonteco@users.noreply.github.com>
2025-12-26 10:45:23 -0500

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -1,1 +1,33 @@
 //jobs.js
+
+// Seasonal mowing frequencies by month
+const mowingCycles = {
+    //fast growth season
+    springSummer: {
+        label: "Spring/Summer",
+        months: ["March", "April", "May", "June", "July", "August", "September", "October"],
+        defaultFrequency: "Weekly"
+        },
+    //slow growth season
+    fallWinter: {
+        label: "Fall/Winter",
+        months: ["November", "December", "January", "February"],
+        defaultFrequency: "biweekly" //can be overwritten to monthly
+        },
+    };
+
+
+//This is our "database" for now
+const jobs = [
+    {
+        id: 1
+        customerName: "Sanja Kosak",
+        jobType: "Mowing",
+        address: "4108 Piney Branch Court",
+        subscriptionType: "Recurring"
+        subscriptionStatus: "Pending",  //Pending | Active | Inactive | On-Time(changes to inactive after complete)
+        frequency: "Auto-Seasonal",  //refers to mowingCycles rules
+        scheduledDate: "2026-01-05",
+        lastCompletedDate: null
+    }
+];
